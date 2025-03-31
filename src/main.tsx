48b1f35de7a2c86153d09d0b1594630492babc17
D33key <dimakisa777@yandex.ru>
2025-03-31 09:53:37 +0300

    remove

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Scene from './Scene.tsx';
+import Scene from './Scene.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
